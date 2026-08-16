7c69cac9adc369c823b4cb873cb3edb58d624f8c
Martijn Kerssing <martijn@kerssing.nl>
2026-08-16 23:11:57 +0200

    refactor: store definitive credits in If Time Had Waited

--- src/content/songs/if-time-had-waited.ts
+++ src/content/songs/if-time-had-waited.ts
@@ -73,8 +73,8 @@ The day I looked at you`,
   ],
   credits: {
     artist: "Northbound Project",
-    writer: "Northbound Project",
-    producer: "Northbound Project",
+    writer: "Martijn Kerssing",
+    producer: "Northbound Project, Open Road Records",
   },
   streamingLinks: [],
 };
