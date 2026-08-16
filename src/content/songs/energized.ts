8c9a54fc9cf63eaf11c13add59dc5e549d7c1c8d
Martijn Kerssing <martijn@kerssing.nl>
2026-08-16 23:10:59 +0200

    refactor: store definitive credits in Energized

--- src/content/songs/energized.ts
+++ src/content/songs/energized.ts
@@ -91,8 +91,8 @@ Only silence knows…`,
   ],
   credits: {
     artist: "Northbound Project",
-    writer: "Northbound Project",
-    producer: "Northbound Project",
+    writer: "Martijn Kerssing",
+    producer: "Northbound Project, Open Road Records",
   },
   streamingLinks: [],
 };
